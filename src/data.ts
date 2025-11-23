76faa7a0cbadf4f5e214a49ba1b0de67f2d9a468
DaroMacs <dario.maceg@gmail.com>
2025-11-23 05:14:16 -0300

    Device data

--- src/data.ts
+++ src/data.ts
@@ -60,3 +60,4 @@ export const gridItems = [
     spareImg: "",
   },
 ];
+
---
+++ src/data/index.ts
@@ -0,0 +1,117 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "Reliable physical data, transparent and secure",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/water-droplet.png",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Use the data anywhere",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "Powered by Mantle",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Providing valuable data to B2B and B2C",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Verified data, always transparent",
+    description: "Support 24/7",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Manage your services",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const technologies = [
+  {
+    img: "/solidity.png",
+  },
+  {
+    img: "/mantle.svg",
+  },
+
+  {
+    img: "/ethereum.svg",
+  },
+  {
+    img: "/OpenZeppelin.svg",
+  },
+];
+
+export const supporters = [
+  {
+    id: 1,
+    name: "odisea",
+    img: "/odisea.png",
+  },
+  {
+    id: 2,
+    name: "mantle",
+    img: "/mantle.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/DaroMacs",
+  },
+  {
+    id: 2,
+    img: "/x.svg",
+    link: "https://x.com/MacsDaro",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/dario-macedo-44818a55/",
+  },
+];
